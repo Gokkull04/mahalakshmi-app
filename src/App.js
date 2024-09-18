704ff5ed8533990996be05bf700c8d223ac7754f
Gokkull04 <gokkull04@gmail.com>
2024-09-18 08:56:11 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PropertyCard from "./PropertyCard";
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      hello
       <PropertyCard />
     </div>
   );
