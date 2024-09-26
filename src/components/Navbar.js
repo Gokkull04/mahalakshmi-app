8c9f27217f29b8a084e7d634c5dc29d3a421022c
Gokkull04 <gokkull04@gmail.com>
2024-09-26 14:45:48 +0530

    home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../img/Mahalakshmi logo.png";
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-900 text-white shadow-lg">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <div className="text-2xl font-bold">
-          <Link to="/">Mahalakshmi Builders</Link>
+      <div className="container mx-auto flex justify-between items-center py-2 px-6">
+        <div className="">
+          <Link to="/"><img src={logo} alt="logo" className="h-14 w-full"></img></Link>
         </div>
         <ul className="flex space-x-6">
           <li>
@@ -15,8 +16,8 @@ const Navbar = () => {
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="hover:text-yellow-400 transition">
-              Contact
+            <Link to="/about" className="hover:text-yellow-400 transition">
+              About
             </Link>
           </li>
         </ul>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-6 text-blue-900 animate-pulse">
-          Welcome to Mahalakshmi Builders
+          Welcome to Mahalakshmi <br></br> Builders and Land Promoters
         </h1>
         <p className="text-lg mb-8 text-gray-700">
           Building your dreams with excellence and integrity.
         </p>
         <Link
-          to="/contact"
+          to="/about"
           className="bg-blue-900 text-white py-3 px-8 rounded-lg hover:bg-yellow-400 hover:text-blue-900 transition"
         >
           Get In Touch
